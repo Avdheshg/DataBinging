62200cc41473e0696d8a72a0022dc2457a40084c
Avdhesh <avdheshg804@gmail.com>
2022-04-06 14:32:55 +0530

    Phone App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import {ContactComponent} from "./contact/contact.component";
+import {PhoneComponent} from "./phone/phone.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    PhoneComponent
   ],
   imports: [
     BrowserModule,
